8932a91cad444385e88650b0a0e609dd3fd20d27
evgenia <evgenia59-007@mail.ru>
2018-02-05 01:12:35 +0300

    запись в файл

---
+++ task_04_01.py
@@ -0,0 +1,20 @@
+with open(r'C:\Users\evgeniya\Documents\work_python\data.txt') as f:
+    numbers = f.read()
+    n = int(input())
+    p = int(input())
+    list1_del_n = []
+    list2_pow_p = []
+    text_list = numbers.split(sep=' ')
+    i = 1
+    for i in range(len(text_list)):
+        if int(text_list[i]) % n == 0:
+            list1_del_n.append(int(text_list[i]))
+        list2_pow_p.append(int(text_list[i])**p)
+        i += 1
+    file1 = open('out-1.txt', 'w')
+    file1.write(str(list1_del_n).replace(',', ' '))
+    file2 = open('out-2.txt', 'w')
+    file2.write(str(list2_pow_p).replace(',', ' '))
+    file1.close()
+    file2.close()
+
