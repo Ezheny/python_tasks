edb127e24b0ae72c424555b96c1b3979fb6da966
evgenia <evgenia59-007@mail.ru>
2018-02-18 00:17:24 +0300

    decorator login

---
+++ task_decorator_login_required.py
@@ -0,0 +1,46 @@
+import hashlib
+
+
+def make_token(username, password):
+    s = username + password
+    # print(s)
+    return hashlib.md5(s.encode()).hexdigest()
+
+
+def save_token(username, password):
+    with open('token.txt', 'w') as f:
+        f.write(make_token(username, password))
+
+
+def get_token():
+    with open('token.txt') as f:
+        info = f.read().strip()
+        # print(info)
+    return info
+
+
+def login_required(func):
+    def wrapper(*args, **kwargs):
+        i = 3
+        while i > 0:
+            user = input()
+            password = input()
+            if get_token() == make_token(user, password):
+                return func(*args, **kwargs)
+            i -= 1
+            return None
+    return wrapper
+
+
+@login_required
+def f1():
+    print('Функция защищена паролем')
+
+
+@login_required
+def f2():
+    print('Эта функция тоже защищена паролем')
+
+
+f1()
+f2()
