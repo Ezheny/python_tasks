2dc512a449d42d9f3f5ca0afecb01fcacf060be9
evgenia <evgenia59-007@mail.ru>
2018-02-17 22:29:56 +0300

    namedtuple

---
+++ task_decorator_return_namedtuple.py
@@ -0,0 +1,22 @@
+from collections import namedtuple
+
+
+def return_namedtuple(*name):
+    def decorator(func):
+        def wrapper(*args):
+            result = func(*args)
+            if isinstance(result, tuple):
+                n_tuple = namedtuple('n_tuple', name)
+                result = n_tuple(*result)
+            return result
+        return wrapper
+    return decorator
+
+
+@return_namedtuple('one', 'two')
+def func():
+    return 1, 2
+
+r = func()
+print(r.one)
+print(r.two)
