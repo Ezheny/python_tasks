c09858e3ab290c2c7264362b0fe89e6a36ee9d22
evgenia <evgenia59-007@mail.ru>
2018-02-18 13:06:59 +0300

    password generator

--- task_generator_password.py
+++ task_generator_password.py
@@ -1,13 +1,13 @@
 from string import digits, ascii_letters
 import random
 
-
 def password_generator(len):
     valid_values = list(digits + ascii_letters)
     random.shuffle(valid_values)
     password = ''.join([random.choice(valid_values) for x in range(len)])
-    # print(password)
-    return password
+    while 1:
+        yield password
 
 
 # password_generator(25)
+
